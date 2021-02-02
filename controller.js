948084fde73b01912f30bd6fd61c1aa25dd47cf9
feb08 <febriaji8299@gmail.com>
2021-02-02 21:36:06 +0700

    menampilkan data mhs berdasarkan id

--- controller.js
+++ controller.js
@@ -17,3 +17,15 @@ exports.tampilsemuamahasiswa = function(req, res) {
         }
     });
 };
+
+//Menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id], function(error, rows, fileds){
+        if(error){
+            console.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
