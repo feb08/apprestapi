9477c145b35a9a0f6ed2dbb5af280e119bdcf220
feb08 <febriaji8299@gmail.com>
2021-02-05 22:40:09 +0700

    nested mantap booorrr

--- controller.js
+++ controller.js
@@ -73,3 +73,14 @@ exports.hapusmahasiswa = function(req, res) {
         }
     });
 };
+
+// Menampilkan matakuliah group
+exports.tampilgroupmahasiswa = function(req, res) {
+    connection.query('SELECT mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, matakuliah.matakuliah, matakuliah.sks from krs JOIN matakuliah JOIN mahasiswa WHERE krs.id_matakuliah = matakuliah.id_matakuliah AND krs.id_mahasiswa = mahasiswa.id_mahasiswa ORDER BY mahasiswa.id_mahasiswa', function(error, rows, fields) {
+        if(error){
+            console.log(error)
+        } else {
+            response.oknested(rows, res)
+        }
+    });
+};
